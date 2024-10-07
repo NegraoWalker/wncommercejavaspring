9d55b6dbf0f873051f6564847810de8ce4a1c85e
walkerNegrão <walker.esteves.negrao@gmail.com>
2024-10-07 14:23:51 -0300

    Criação do repositório da aplicação

---
+++ src/test/java/com/walker/wncommercejavaspring/WncommercejavaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walker.wncommercejavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WncommercejavaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
