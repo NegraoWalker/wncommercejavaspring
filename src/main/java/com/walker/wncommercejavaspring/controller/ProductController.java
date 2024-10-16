79c815fcae2e7e88d62a919d7014837df9fa0f20
Walker Negrão <c0662240@vale.com>
2024-10-16 09:16:24 -0300

    Teste do uso do ProductRepository

--- src/main/java/com/walker/wncommercejavaspring/controller/ProductController.java
+++ src/main/java/com/walker/wncommercejavaspring/controller/ProductController.java
@@ -1,18 +1,27 @@
 package com.walker.wncommercejavaspring.controller;
 
+import com.walker.wncommercejavaspring.model.Product;
+import com.walker.wncommercejavaspring.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products") //Rota /products
 public class ProductController {
 
+    @Autowired
+    private ProductRepository productRepository;
 
 
 
     @GetMapping
     public String test() {
-        return "Hello World!";
+        Optional<Product> result = productRepository.findById(1L);
+        Product product = result.get();
+        return product.getName();
     }
 }
